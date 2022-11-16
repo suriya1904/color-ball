902b84d142bc5f3c1c01c6cd0474b914d5ffbd30
suriya1904 <suriya79044@gmail.com>
2022-11-16 14:42:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function red(){
+    document.querySelector("div").style.background= "red";
+}
+function color (value){
+    document.querySelector("div").style.background= value;
+}
+function colorInput (){
+    let color= document.getElementById("colorinput").value;
+    document.querySelector("div").style.background= color;
+}
